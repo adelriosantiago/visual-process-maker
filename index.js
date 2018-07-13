b99f6765d2f205c09537781ffac6aaaa30d1f792
Alejandro del Río <adelriosantiago@gmail.com>
2018-07-12 21:57:02 -0500

    Add template Fix views Add EJS

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express()
 const port = 3777
 
 //Set the view engine to ejs
+app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(bodyParser.json()) //To support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) //To support URL-encoded bodies
 app.use(express.static('public'))
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(port, (err) => {
